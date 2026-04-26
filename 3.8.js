93578b4e2e04390ee3cde5238ecedaf5c8c12228
Ariel dos Santos de Cristo Góes <santosng456@gmail.com>
2026-04-26 17:36:10 -0300

    alterações

--- 3.8.js
+++ 3.8.js
@@ -13,14 +13,21 @@ function perguntar(pergunta) {
   });
 }
 
-async function main () {
-    let x = 1;
-
-    for(let i =1; i<=10; i++){
-        console.log(`5 x ${i} = ${5 * i}` );
+async function main() {
     
-    }
+    let A = 1
+    let B = 1
 
+    console.log(A);
+    console.log(B);
+
+    for(let i= 1; i<=30; i++){
+        let proximo= A+B
+        console.log(proximo);
+
+        A=B;
+        B=proximo;
+    }
     rl.close();
 }
 main();
